8a9c90aeabaf957dc008399702e34d7240604b6d
Adam <adamlederer@live.ca>
2023-02-21 19:07:15 -0700

    added debugger

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
 from dbhelpers import run_statement
 from dbcreds import production_mode
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.get('/api/client')
 def get_clients():
-    result = run_statement("CALL get_client")
+    id = request.args.get('clientId')
+    result = run_statement("CALL get_client(?)", [id])
+    keys = ["username", "firstName", "lastName", "email", "createdAt"]
+    response = []
+    if (type(result) == list):
+        for clients in result:
+            response.append(dict(zip(keys, clients)))
+        return make_response(jsonify(response), 200)
+    else:
+        return make_response(jsonify(result), 500)
 
 
-
+# Client post is creating a token.
+# Creates user and logs in user, in 2 procedure calls in 1 python
+# Procedure can be used with another procedure.
+# Call procedures within procedures.
 
 if (production_mode == True):
     print("Running server in production mode")
